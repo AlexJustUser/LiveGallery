fa3154e20bc8814dd881a8b2ee74941d535098cd
alex <swimmersuper16@gmail.com>
2021-08-05 01:06:11 +0300

    Initial commit

---
+++ app/src/main/java/com/maveri/livegallery/view/MainFragment.java
@@ -0,0 +1,16 @@
+package com.maveri.livegallery.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.maveri.livegallery.R;
+
+public class MainFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
