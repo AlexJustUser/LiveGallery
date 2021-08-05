abeb984cbf3033e55fadb8b17988c8c5010e6a9b
alex <swimmersuper16@gmail.com>
2021-08-05 14:58:45 +0300

    Minor changes

--- app/src/main/java/com/maveri/livegallery/api/model/Image.java
+++ app/src/main/java/com/maveri/livegallery/api/model/Image.java
@@ -1,77 +1,16 @@
 package com.maveri.livegallery.api.model;
 
+import com.google.gson.internal.LinkedHashTreeMap;
+
 public class Image {
-    private String url;
-    private String width;
-    private String height;
-    private String size;
-    private String mp4;
-    private String mp4_size;
-    private String webp;
 
-    public void setUrl(String url) {
-        this.url = url;
+    private LinkedHashTreeMap fixed_height;
+
+    public void setFixed_height(LinkedHashTreeMap fixed_height) {
+        this.fixed_height = fixed_height;
     }
 
-    public void setWidth(String width) {
-        this.width = width;
+    public LinkedHashTreeMap getFixed_height() {
+        return fixed_height;
     }
-
-    public void setHeight(String height) {
-        this.height = height;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public void setMp4(String mp4) {
-        this.mp4 = mp4;
-    }
-
-    public void setMp4_size(String mp4_size) {
-        this.mp4_size = mp4_size;
-    }
-
-    public void setWebp(String webp) {
-        this.webp = webp;
-    }
-
-    public void setWebp_size(String webp_size) {
-        this.webp_size = webp_size;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public String getWidth() {
-        return width;
-    }
-
-    public String getHeight() {
-        return height;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public String getMp4() {
-        return mp4;
-    }
-
-    public String getMp4_size() {
-        return mp4_size;
-    }
-
-    public String getWebp() {
-        return webp;
-    }
-
-    public String getWebp_size() {
-        return webp_size;
-    }
-
-    private String webp_size;
 }
--- app/src/main/java/com/maveri/livegallery/ui/MainActivity.java
+++ app/src/main/java/com/maveri/livegallery/ui/MainActivity.java
@@ -1,21 +1,31 @@
 package com.maveri.livegallery.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.maveri.livegallery.api.model.Gif;
 import com.maveri.livegallery.api.model.GifResponse;
 import com.maveri.livegallery.databinding.ActivityMainBinding;
 import com.maveri.livegallery.presenter.GifPresenter;
 import com.maveri.livegallery.presenter.IGifPresenter;
 
-public class MainActivity extends AppCompatActivity implements MainView{
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MainView, MyRecyclerViewAdapter.ItemClickListener{
 
     private ActivityMainBinding binding;
     private IGifPresenter presenter;
+    private EndlessRecyclerViewScrollListener scrollListener;
+    private MyRecyclerViewAdapter adapter;
+
+    private static final int DEFAULT_PAGE_NUMBER  = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,18 @@ public class MainActivity extends AppCompatActivity implements MainView{
     private void init(){
 
         presenter = new GifPresenter(this, this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        binding.listOfGifs.setLayoutManager(linearLayoutManager);
+
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                loadNextDataFromApi(++page);
+            }
+        };
+        binding.listOfGifs.addOnScrollListener(scrollListener);
+
+        presenter.getDefaultGifs("SVBkGciuiJJucO12lztv14fJ7lIdcGJ8", 10, DEFAULT_PAGE_NUMBER, "g", "e826c9fc5c929e0d6c6d423841a282aa");
 
         binding.gifSearch.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -44,11 +66,37 @@ public class MainActivity extends AppCompatActivity implements MainView{
                 return false;
             }
         });
-        presenter.getDefaultGifs("SVBkGciuiJJucO12lztv14fJ7lIdcGJ8", 20, 5, "g", "e826c9fc5c929e0d6c6d423841a282aa");
+    }
+
+    public void loadNextDataFromApi(int pageNum) {
+        presenter.getDefaultGifs("SVBkGciuiJJucO12lztv14fJ7lIdcGJ8", 10, pageNum, "g", "e826c9fc5c929e0d6c6d423841a282aa");
     }
 
     @Override
     public void displayMapsList(GifResponse gifResponse) {
+        //if(dataResponse.getPage().equals("1")){
+            //mapsList = dataResponse.getData();
+            List<Gif> gifs = Arrays.asList(gifResponse.getData());
+            adapter = new MyRecyclerViewAdapter(this, gifs);
+            adapter.setClickListener(this);
+            try {
+                binding.listOfGifs.setAdapter(adapter);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+//        }
+//        else{
+//            for(MapResponse map: dataResponse.getData()){
+//                mapsList.add(map);
+//            }
+//            adapter.updateData(mapsList);
+//            adapter.notifyItemInserted(mapsList.size()-dataResponse.getData().size());
+//        }
+    }
+
+    @Override
+    public void onItemClick(View view, Gif gifItem) {
 
     }
 }
